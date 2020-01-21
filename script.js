8210c02d7b788c648914b6df36620f004805227b
halvorheienxd <60137744+halvorheienxd@users.noreply.github.com>
2020-01-21 23:29:58 +0100

    ye

---
+++ script.js
@@ -0,0 +1,22 @@
+const h1 = document.querySelector('h1');
+const h3 = document.querySelector('h3');
+
+
+function display()
+{
+   const today = new Date();
+   const day = today.getDate();
+   const month = today.getMonth() + 1;
+   const year = today.getFullYear();
+   const hour = today.getHours() > 24 ? today.getHours() - 12 : today.getHours();
+   const minute = today.getMinutes();
+   const seconds = today.getSeconds();
+
+   const output = day + '/' + month + '-' + year;
+   const output2 = hour + ':' + minute + ':' + seconds;
+
+
+   h1.textContent = output;
+   h3.textContent = output2;
+}
+setInterval(display, 1000);
