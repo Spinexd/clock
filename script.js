66e0abe1947ba22386044c4335c619beb0c95bff
halvorheienxd <60137744+halvorheienxd@users.noreply.github.com>
2020-01-22 15:39:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+
 function display() {
 
   const time = new Date();
-  document.querySelector('h3').innerHTML = time.toUTCString();
+
+  document.querySelector('h3').innerHTML = time.toUTCString() > 12 ? today.getHours() - 12 : today.getHours();
+
+
 
 } setInterval(display, 1000);
