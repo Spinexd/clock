06dc7d37222eb9ca5f715514b173aace93558b94
halvorheienxd <60137744+halvorheienxd@users.noreply.github.com>
2020-01-22 06:12:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -16,8 +16,8 @@ function display()
    const output = day + '/' + month + '-' + year;
    const output2 = hour + ':' + minute + ':' + seconds;
 
-   getMinutes = (getMinutes < 10 ? "0" : "")  + getMinutes;
-   getSeconds = ( getSeconds < 10 ? "0" : "" ) + getSeconds;
+   getMinutes = (getMinutes ≤ 10 ? "0" : "")  + getMinutes;
+   getSeconds = ( getSeconds ≤ 10 ? "0" : "" ) + getSeconds;
    h1.textContent = output;
    h3.textContent = output2;
 }
