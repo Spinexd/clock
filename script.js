81bec09040b15132090e2bb9f1aebec673fab616
halvorheienxd <60137744+halvorheienxd@users.noreply.github.com>
2020-01-22 11:13:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const h3 = document.querySelector('h3');
 
 function display()
 {
-   const today = new Date(Date.UTC(2012, 11, 20, 3, 0, 0));
+   const today = new Date(Date.UTC(today.getMiutes(), today.getSeconds()));
    const day = today.getDate();
    const month = today.getMonth() + 1;
    const year = today.getFullYear();
