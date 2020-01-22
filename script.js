d6eefb1718f73faef93ae299e63dc245818052c5
halvorheienxd <60137744+halvorheienxd@users.noreply.github.com>
2020-01-22 11:26:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const h3 = document.querySelector('h3');
 
 function display()
 {
-   const today = (new Date()).toLocaleDateString('en-GB', {minute: '2-digit', seconds: '2-digit'});
-  /* const day = today.getDate(); */
+   const today = (new Date()).toLocaleDateString('en-GB', {minute: '2-digit', seconds: '2-digit};
+   const day = today.getDate();
    const month = today.getMonth() + 1;
    const year = today.getFullYear();
    const hour = today.getHours() > 24 ? today.getHours() - 12 : today.getHours();
